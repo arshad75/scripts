38beeb58aafbb41a067c7fdd7382f623910f9802
Arshad Syed <arssyed@publicisgroupe.net>
2023-06-02 01:29:16 +0530

    adding script

--- devx.sh
+++ devx.sh
@@ -4,3 +4,7 @@ mkdir .devcontainer/
 cd .devcontainer/
 
 #install dockerfile and devcontainer.json
+curl https://raw.githubusercontent.com/arshad75/scripts/main/.devcontainer/Dockerfile >> Dockerfile
+curl https://raw.githubusercontent.com/arshad75/scripts/main/.devcontainer/devcontainer.json >> devcontainer.json
+
+
